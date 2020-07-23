dcb65aa47714f74a38add48cf4b51239df1976ad
jaweria332 <61407664+jaweria332@users.noreply.github.com>
2020-07-24 00:34:27 +0500

    some very basic JS program

--- conversion_f_c.js
+++ conversion_f_c.js
@@ -1,8 +1,18 @@
 
-function to_fahrenheit(temp){
-return (temp * (9/5)) + 32;
+function temp_convert(temp, scale){
+  if((scale == "celsius") || (scale == "C"))  //given temperature is in Celsius scale
+  {
+    return (temp * 9/5) + 32 + " degree F";
+  }
+
+else if((scale == "fahrenheit") || (scale == "F"))    //given temperature is in Fehrenheit scale
+{
+  return (temp -32) * 5 / 9 + " degree celsius";
+}
+  else {
+    return "Invalid scale";
+  }
 }
 
-function to_celsius(temp){
-return ((temp -32) * 5 )/9;
-}
+temp_convert(100, "C");
+temp_convert(212, "F");
